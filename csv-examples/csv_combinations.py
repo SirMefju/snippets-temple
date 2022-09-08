c177af16e7af41516833cbd85a95b1a2c01145fe
sirmefju <mat.cieslik98@gmail.com>
2022-09-08 20:45:38 +0200

    csv modify

--- csv-examples/csv_combinations.py
+++ csv-examples/csv_combinations.py
@@ -1,22 +1,32 @@
 import csv
 
-def csv_writer(file):
-    with open(file, 'w', newline='') as csvfile:
-        field_names = ['Name', 'SecondName']
-        writer = csv.DictWriter(csvfile, fieldnames=field_names)
-        dict_with_values = {
-            'Name' : 'Jean-Luc',
-            'SecondName' : 'Picard'
-        }
-        writer.writerow(dict_with_values)
 
-def csv_reader(file):
-    with open(file, 'r', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for i in spamreader:
-            print(i)
+class CsvCombinations:
+    def csv_writer(file):
+        with open(file, 'w', newline='') as csvfile:
+            field_names = ['Name', 'SecondName']
+            writer = csv.DictWriter(csvfile, fieldnames=field_names)
+            dict_with_values = {
+                'Name' : 'Jean-Luc',
+                'SecondName' : 'Picard'
+            }
+            writer.writerow(dict_with_values)
+
+    def csv_reader(file):
+        with open(file, 'r', newline='') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            results = []
+            for i in spamreader:
+                dict_with_results = {
+                    'Name' : i[0],
+                    'SecondName' : i[1]
+                }
+                results.append(dict_with_results)
+            for i in results:
+                print(i)
+                print(i['Name'])
+
 
 if __name__ == '__main__':
-    csv_writer('file.csv')
-    csv_reader('file.csv')
-    print('done')
+    CsvCombinations.csv_writer('csv-examples/file.csv')
+    CsvCombinations.csv_reader('csv-examples/file.csv')
